e6a44dd0a57d5391801c2b4161b0843f486be2a7
Narek Avanesyan <avanesyan12345@gmail.com>
2025-10-14 22:23:48 +0400

    Get category details to edit.

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -39,5 +39,32 @@ namespace BulkyWeb.Controllers
 
             return View();
         }
+
+        public IActionResult Edit(int id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            Category? category = _db.categories.Find(id);       // Find() works only on primary key.
+            //Category? category = _db.categories.FirstOrDefault(u => u.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View();
+        }
     }
 }
