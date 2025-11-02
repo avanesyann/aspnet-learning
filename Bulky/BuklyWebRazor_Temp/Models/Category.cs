261e7994ba8d7e688a3fb0f060d022a8cf952cf5
Narek Avanesyan <avanesyan12345@gmail.com>
2025-11-02 23:27:45 +0400

    Set up the Razor project.

---
+++ Bulky/BuklyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        // if the name is Id (or something like CategoryId),
+        // the entity framework core will think it's a primary key by default (no need for data annotation)
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
