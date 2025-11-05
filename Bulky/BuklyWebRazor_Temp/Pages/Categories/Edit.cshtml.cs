b8db3c6b54bb6f17b7bfbdf3c45fe6422b5db4fb
Narek Avanesyan <avanesyan12345@gmail.com>
2025-11-05 17:13:36 +0400

    Add Edit functionality. (Razor)

---
+++ Bulky/BuklyWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,36 @@
+using BuklyWebRazor_Temp.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BuklyWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        public Category Category { get; set; }
+
+        public EditModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void OnGet(int? id)
+        {
+            if (id != null & id != 0)
+            {
+                Category = _context.Categories.Find(id);
+            }
+        }
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(Category);
+                _context.SaveChanges();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
