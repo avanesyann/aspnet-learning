198f02dcb3e42e0b177d6d5cc8d642c90513aa73
Narek Avanesyan <avanesyan12345@gmail.com>
2025-11-19 23:40:39 +0400

    Create StockController.

---
+++ FinShark/FinShark/Controllers/StockController.cs
@@ -0,0 +1,35 @@
+﻿using FinShark.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+
+namespace FinShark.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public StockController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stocks.ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if (stock == null)
+                return NotFound();
+
+            return Ok(stock);
+        }
+    }
+}
