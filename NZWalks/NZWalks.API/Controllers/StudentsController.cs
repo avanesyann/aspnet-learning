3f57d795d8991ffee2f1b7b30a5ca06842101864
Narek Avanesyan <avanesyan12345@gmail.com>
2025-11-23 17:36:41 +0400

    Add students conroller.

---
+++ NZWalks/NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:7192/api/Students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: https://localhost:7192/api/Students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            // for demonstration purposes only, assume this is a database
+            string[] studentNames = new string[] { "John", "Jane", "Mark", "Emily" };
+
+            return Ok(studentNames);    // successful 200 response
+        }
+    }
+}
