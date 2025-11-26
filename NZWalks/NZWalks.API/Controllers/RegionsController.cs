e8c5a5f44958e0544e1e6368ab99318f67229c61
Narek Avanesyan <avanesyan12345@gmail.com>
2025-11-26 23:46:45 +0400

    Create GetAll() action method.

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:7192/api/Regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RegionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: https://localhost:7192/api/Regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = _context.Regions.ToList();
+
+            return Ok(regions);
+        }
+    }
+}
