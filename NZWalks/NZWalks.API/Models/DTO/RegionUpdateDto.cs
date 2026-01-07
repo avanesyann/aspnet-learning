7955ac05f419c94803b757a954081a0a748d031a
Narek Avanesyan <avanesyan12345@gmail.com>
2026-01-07 23:10:27 +0400

    Validate Regions Update action.

--- NZWalks/NZWalks.API/Models/DTO/RegionUpdateDto.cs
+++ NZWalks/NZWalks.API/Models/DTO/RegionUpdateDto.cs
@@ -1,8 +1,15 @@
-﻿namespace NZWalks.API.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.API.Models.DTO
 {
     public class RegionUpdateDto
     {
+        [Required]
+        [MinLength(3, ErrorMessage = "Minimum of 3 characters required."), MaxLength(5, ErrorMessage = "Maximum of 5 characters required.")]
         public string Code { get; set; }
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "Maximum of 100 characters required.")]
         public string Name { get; set; }
         public string? ImageUrl { get; set; }
     }
