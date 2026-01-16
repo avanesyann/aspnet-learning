ef7bca6fbbadbb17ce11d7d946da885dd0ec30e1
Narek Avanesyan <avanesyan12345@gmail.com>
2026-01-16 23:40:42 +0400

    Add ToDo Model.

---
+++ ToDoList/ToDoList.API/Model/ToDo.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoList.API.Model
+{
+    public class ToDo
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
