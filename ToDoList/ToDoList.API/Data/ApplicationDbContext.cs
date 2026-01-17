c806e35b47e40699b0886d96f2f9db58a8310da9
Narek <avanesyan12345@gmail.com>
2026-01-17 17:15:06 +0400

    Add ToDos DbSet to DB.

--- ToDoList/ToDoList.API/Data/ApplicationDbContext.cs
+++ ToDoList/ToDoList.API/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.API.Model;
 
 namespace ToDoList.API.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<ToDo> ToDos { get; set; }
     }
 }
--- ToDoList/ToDoList.API/Model/ToDo.cs
+++ ToDoList/ToDoList.API/Model/ToDo.cs
@@ -3,9 +3,9 @@
     public class ToDo
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
         public bool IsCompleted { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
