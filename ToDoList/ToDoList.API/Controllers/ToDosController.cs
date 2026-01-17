92b22e59673ce9408869a54d388367929e7e8c93
Narek <avanesyan12345@gmail.com>
2026-01-17 17:43:02 +0400

    Create GetAll() ToDos action method.

---
+++ ToDoList/ToDoList.API/Controllers/ToDosController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.API.Data;
+using ToDoList.API.Model;
+
+namespace ToDoList.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ToDosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var domainToDos = _context.ToDos.ToList();
+            var dtoToDos = new List<ToDoReadDto>();
+
+            if (domainToDos == null)
+            {
+                return NotFound();
+            }
+
+            foreach (var toDo in domainToDos)
+            {
+                dtoToDos.Add(new ToDoReadDto
+                {
+                    Id = toDo.Id,
+                    Name = toDo.Name,
+                    Description = toDo.Description,
+                    IsCompleted = toDo.IsCompleted,
+                    CreatedAt = toDo.CreatedAt
+                });
+            }
+
+            return Ok(dtoToDos);
+        }
+    }
+}
---
+++ ToDoList/ToDoList.API/Model/ToDoReadDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoList.API.Model
+{
+    public class ToDoReadDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
