1fa9e7977828ebac753a9be5089ab921207079dc
Narek <avanesyan12345@gmail.com>
2026-02-04 23:40:05 +0400

    Seed identity roles to Db.

--- NZWalks/NZWalks.API/Data/AuthenticationDbContext.cs
+++ NZWalks/NZWalks.API/Data/AuthenticationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZWalks.API.Data
@@ -6,5 +7,33 @@ namespace NZWalks.API.Data
     public class AuthenticationDbContext : IdentityDbContext
     {
         public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "ad1fa31d-ae11-4a80-a4d5-efec0bc0d6ef";
+            var writerRoleId = "957da2ce-02bf-4312-8420-1bfdd3a8e7a4";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
