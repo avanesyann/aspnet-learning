0b1a6cf1ac22e7d71bff58fa276a1a1d2d3ec4a0
Narek <avanesyan12345@gmail.com>
2026-02-24 16:27:05 +0400

    Create Image domain model.

---
+++ NZWalks/NZWalks.API/Models/Domain/Image.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NZWalks.API.Models.Domain
+{
+    public class Image
+    {
+        public Guid Id { get; set; }
+        [NotMapped]     // Specifies that the property not to be included in migrations.
+        public IFormFile File { get; set; }
+        public string FileName { get; set; }
+        public string? FileDescription { get; set; }
+        public string FileExtension { get; set; }
+        public long FileSizeInBytes { get; set; }
+        public string FilePath { get; set; }
+    }
+}
