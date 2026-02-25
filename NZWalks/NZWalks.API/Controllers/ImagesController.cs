ebdbd09a2310e713573722ad8765dcbe5321ecf9
Narek <avanesyan12345@gmail.com>
2026-02-25 16:48:30 +0400

    Create ImageUploadDto.

---
+++ NZWalks/NZWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        // POST: /api/Images/Upload
+        // [HttpPost]
+        // [Route("Upload")]
+        // public async Task<IActionResult> Upload() { }
+    }
+}
