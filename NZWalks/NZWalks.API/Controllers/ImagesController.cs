3d32ad483b5e03657e26129bd3345dd36972ca79
Narek <avanesyan12345@gmail.com>
2026-02-26 11:10:47 +0400

    Add validation for images.

--- NZWalks/NZWalks.API/Controllers/ImagesController.cs
+++ NZWalks/NZWalks.API/Controllers/ImagesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
 
 namespace NZWalks.API.Controllers
 {
@@ -8,8 +9,33 @@ namespace NZWalks.API.Controllers
     public class ImagesController : ControllerBase
     {
         // POST: /api/Images/Upload
-        // [HttpPost]
-        // [Route("Upload")]
-        // public async Task<IActionResult> Upload() { }
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadDto request)
+        {
+            ValidateFileUpload(request);
+
+            if (ModelState.IsValid)
+            {
+                // Use repository to upload image
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(ImageUploadDto request)
+        {
+            var allowedExtension = new string[] { ".jpg", ".jpeg", ".png" };
+
+            if (!allowedExtension.Contains(Path.GetExtension(request.File.FileName)))
+            {
+                ModelState.AddModelError("file", "Unsupported extension");
+            }
+
+            if (request.File.Length > 1048576)
+            {
+                ModelState.AddModelError("file", "File size exceeds 10MB");
+            }
+        }
     }
 }
