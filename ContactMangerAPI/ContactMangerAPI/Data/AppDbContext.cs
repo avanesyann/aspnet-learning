7999547df95de99a03e7a996c1d4136513ff6aab
Narek Avanesyan <avanesyan12345@gmail.com>
2026-03-07 18:00:00 +0400

    Set up ContactManager project.

---
+++ ContactMangerAPI/ContactMangerAPI/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ContactMangerAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
