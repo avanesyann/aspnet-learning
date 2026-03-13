22c65b2eb49a6e3c91724c375b34e64e75016601
Narek Avanesyan <avanesyan12345@gmail.com>
2026-03-13 23:09:25 +0400

    Add Contact Create DTO Validation.

--- ContactMangerAPI/ContactMangerAPI/Models/DTO/ContactCreateDto.cs
+++ ContactMangerAPI/ContactMangerAPI/Models/DTO/ContactCreateDto.cs
@@ -5,10 +5,14 @@ namespace ContactMangerAPI.Models.DTO
     public class ContactCreateDto
     {
         [Required]
+        [MinLength(2, ErrorMessage = "Minimum of 2 characters required.")]
         public string Name { get; set; }
         [Required]
+        [MaxLength(15, ErrorMessage = "Maximum length should be 15 digits.")]
         public string Phone { get; set; }
+        [StringLength(150)]
         public string? Email { get; set; }
+        [StringLength(250)]
         public string? Address { get; set; }
     }
 }
