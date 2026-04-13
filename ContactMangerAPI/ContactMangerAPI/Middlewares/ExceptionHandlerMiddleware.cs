cae2de714e0f156bfef38b5c2ffab40065cf9a9c
Narek Avanesyan <avanesyan12345@gmail.com>
2026-04-13 22:59:28 +0400

    Create ExceptionHandlerMiddleware.

---
+++ ContactMangerAPI/ContactMangerAPI/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,13 @@
+﻿namespace ContactMangerAPI.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+        private readonly RequestDelegate _next;
+        public ExceptionHandlerMiddleware(ILogger<ExceptionHandlerMiddleware> logger, RequestDelegate next)
+        {
+            _logger = logger;
+            _next = next;
+        }
+    }
+}
