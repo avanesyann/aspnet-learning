5f059b34579107cc5bf1a0cfe4984a2e2f2fcf6a
Narek Avanesyan <avanesyan12345@gmail.com>
2026-04-16 23:23:55 +0400

    Inject Middleware.

--- ContactMangerAPI/ContactMangerAPI/Program.cs
+++ ContactMangerAPI/ContactMangerAPI/Program.cs
@@ -1,5 +1,6 @@
 using ContactMangerAPI.Data;
 using ContactMangerAPI.Mappings;
+using ContactMangerAPI.Middlewares;
 using ContactMangerAPI.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -38,6 +39,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionHandlerMiddleware>();
 
 app.UseHttpsRedirection();
 
