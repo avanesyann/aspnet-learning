78e9c298cb076db3eb41b36a288ae437fb5db1cb
Narek Avanesyan <avanesyan12345@gmail.com>
2026-05-02 21:25:54 +0400

    Create Expense Model.

---
+++ ExpenseTracker/ExpenseTracker/Models/Expense.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Description { get; set; } = null!;
+        [Required]
+        [Range(0.01, double.MaxValue)]
+        public decimal Amount { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        public string? Category { get; set; }
+    }
+}
