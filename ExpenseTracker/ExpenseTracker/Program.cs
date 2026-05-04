61e5c3d4c54e265cb0e14879082d2444ea0b4ac1
Narek Avanesyan <avanesyan12345@gmail.com>
2026-05-04 23:22:35 +0400

    Create DbContext.

--- ExpenseTracker/ExpenseTracker/Program.cs
+++ ExpenseTracker/ExpenseTracker/Program.cs
@@ -1,3 +1,6 @@
+using ExpenseTracker.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace ExpenseTracker
 {
     public class Program
@@ -8,6 +11,8 @@ namespace ExpenseTracker
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
