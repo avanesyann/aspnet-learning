83f4e3881e0405781d44cf03e09813fbd375c5f4
Narek Avanesyan <avanesyan12345@gmail.com>
2026-05-09 18:00:00 +0400

    Get a list of Expenses.

--- ExpenseTracker/ExpenseTracker/Controllers/ExpenseController.cs
+++ ExpenseTracker/ExpenseTracker/Controllers/ExpenseController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExpenseTracker.Data;
+using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ExpenseTracker.Controllers
 {
     public class ExpenseController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ExpenseController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            List<Expense> expenses = _context.Expenses.ToList();
+
             return View();
         }
     }
