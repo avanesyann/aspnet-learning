f7d79532ed58caad0a1069d64082c2dd8fe1b78c
Narek <avanesyan12345@gmail.com>
2026-06-08 23:07:06 +0400

    Add IRepository Get and GetAll.

--- ExpenseTracker/ExpenseTracker.DataAccess/Repository/IRepository.cs
+++ ExpenseTracker/ExpenseTracker.DataAccess/Repository/IRepository.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ExpenseTracker.DataAccess.Repository
 {
-    internal interface IRepository<T> where T : class { }
+    internal interface IRepository<T> where T : class
+    {
+        // T - Category
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+    }
 }
