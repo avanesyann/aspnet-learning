f50ffa7ea25bcd44048053b1c9445f47907e955e
Narek <avanesyan12345@gmail.com>
2026-06-11 18:00:00 +0400

    Implement Add()

--- ExpenseTracker/ExpenseTracker.DataAccess/Repository/Repository.cs
+++ ExpenseTracker/ExpenseTracker.DataAccess/Repository/Repository.cs
@@ -4,15 +4,25 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using ExpenseTracker.DataAccess.Data;
 using ExpenseTracker.DataAccess.Repository;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExpenseTracker.DataAccess.Repository
 {
     public class Repository<T> : IRepository<T> where T : class
     {
+        private readonly ApplicationDbContext _context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+
         public void Add(T entity)
         {
-            throw new NotImplementedException();
+            _context.Add(entity);
         }
 
         public T Get(Expression<Func<T, bool>> filter)
