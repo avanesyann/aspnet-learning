b2d71cdb177daf134a0a2d34f42cd8f7af0a3159
Narek <avanesyan12345@gmail.com>
2026-06-15 22:05:59 +0400

    Create IExpenseRepository.

---
+++ ExpenseTracker/ExpenseTracker.DataAccess/Repository/IExpenseRepository.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.DataAccess.Repository
+{
+    public interface IExpenseRepository : IRepository<Expense>
+    {
+
+    }
+}
