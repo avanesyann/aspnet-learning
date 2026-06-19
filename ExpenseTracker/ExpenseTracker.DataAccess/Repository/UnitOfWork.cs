2edcfacf5d9d648ca1a750b233a7d1dceff736b4
Narek <avanesyan12345@gmail.com>
2026-06-19 22:46:28 +0400

    Create UnitOfWork.

---
+++ ExpenseTracker/ExpenseTracker.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using ExpenseTracker.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public IExpenseRepository ExpenseRepository { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            ExpenseRepository = new ExpenseRepository(context);
+        }
+
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
